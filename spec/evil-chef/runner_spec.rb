ee0b03bc14372b4ac8a581e0ef537be6af62d33a
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-12-18 13:49:46 +0200

    changed spec to use subject

--- spec/evil-chef/runner_spec.rb
+++ spec/evil-chef/runner_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 require 'evil-chef'
 
 describe EvilChef::Runner do
-	before(:each) do
-		@runner = EvilChef::Runner.new
-	end
+	subject(:runner) {EvilChef::Runner.new}
+
 	describe "#recipe_eval" do
 		it "should run a recipe successfully" do
 			FileUtils.rm "/tmp/test.txt" if File.exists? "/tmp/test.txt" 
-			@runner.recipe_eval do
+			runner.recipe_eval do
 				log "TEst"
 				file "/tmp/test.txt" do
 					content "TEST"
@@ -17,7 +16,7 @@ describe EvilChef::Runner do
 			expect(File.read("/tmp/test.txt")).to eq("TEST")
 		end
 		it "`recipe_eval` should return a run_status object" do
-			run_status = @runner.recipe_eval do
+			run_status = runner.recipe_eval do
 				log "TEST"
 			end
 			expect(run_status).to be_kind_of(Chef::RunStatus)
@@ -25,7 +24,7 @@ describe EvilChef::Runner do
 
 		it "should throw an exception when the recipe fails" do
 			expect do
-				@runner.recipe_eval do
+				runner.recipe_eval do
 					dlksdsjldsf
 				end
 			end.to raise_error(NameError)
@@ -33,6 +32,6 @@ describe EvilChef::Runner do
 	end
 
 	it "should expose the node object" do
-		expect(@runner.node).to be_kind_of(Chef::Node)
+		expect(runner.node).to be_kind_of(Chef::Node)
 	end
 end
